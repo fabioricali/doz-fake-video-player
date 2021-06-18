14fb4133295597e75971494652285fadca15cce9
Fabio Ricali <f.ricali@xdevel.com>
2021-06-18 12:43:15 +0200

    BUILD

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,4 @@
-// [DozFakeVideoPlayer]  Build version: 1.0.0  
+// [DozFakeVideoPlayer]  Build version: 1.0.1  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory(require("doz"));
